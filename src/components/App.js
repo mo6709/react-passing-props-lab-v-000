d690f5d7087cb9f350df0a82da40b108a3c3d7c8
Moshe Levi <mo6709@gmail.com>
2017-11-09 11:12:39 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,58 @@ import React from 'react';
 
 import FruitBasket from './FruitBasket';
 
-const App = () => <FruitBasket />;
+class App extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      fruit: [],
+      filters: [],
+      currentFilter: null
+    }
+  }
+  
+  componentWillMount() {
+    this.fetchFilters();
+    this.fetchFruit();
+  }
+  
+  fetchFruit = () =>  {
+    fetch('/api/fruit')
+      .then(response => response.json())
+      .then(fruit => this.setState({ fruit }));
+  }
+
+  fetchFilters = () => {
+    fetch('/api/fruit_types')
+      .then(response => response.json())
+      .then(filters => this.setState({ filters }));
+  }
+
+  updateFilter = event => {
+    console.log('new filter: ', event.target.value);
+    this.setState({ currentFilter: event.target.value });
+  }
+
+  render(){
+    return(
+      <div>
+        <FruitBasket 
+          fruit={this.state.fruit}
+          filters={this.state.filters}
+          currentFilter={this.state.currentFilter}
+          updateFilterCallback ={this.updateFilter}   
+        />
+      </div>
+    );
+  }
+}
+
+App.defaultProps = {
+  fruit: [],
+  filters: [],
+  currentFilter: null,
+  updateFilterCallback: ()=>{}
+}
 
 export default App;
